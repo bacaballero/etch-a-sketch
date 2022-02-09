8e56d0f1e859da7f824fb7252f26c5dc17b256aa
brent <brentcaballero5@gmail.com>
2022-02-09 00:03:34 -0600

    add draw functionality to the grid

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 let numberOfDivs = 16;
+let mouseDown = false;
+let mouseUp = true;
 
 const grid = document.querySelector('.grid');
+document.body.onmousedown = () => {
+    mouseDown = true;
+    mouseUp = false;
+};
+document.body.onmouseup = () => {
+    mouseUp = true;
+    mouseDown = false;
+};
 
 function makeGrid(rows, cols) {
     grid.style.setProperty('--grid-rows', rows);
@@ -11,4 +21,21 @@ function makeGrid(rows, cols) {
     };
 };
 
+function draw() {
+    const cells = document.querySelectorAll('.grid-item');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            if (!mouseUp) {
+                cell.style.backgroundColor = "black";
+            };
+        });
+    });
+    cells.forEach((cell) => {
+        cell.addEventListener('click', () => {
+            cell.style.backgroundColor = "black";
+        })
+    })
+};
+
 makeGrid(16, 16); 
+draw();
